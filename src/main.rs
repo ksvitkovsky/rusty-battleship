971beea019720f146721c6405bed42d0cd4f9142
ksvitkovsky <15626295+ksvitkovsky@users.noreply.github.com>
2023-11-24 13:13:16 +0000

    Add websockets sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{net::TcpListener, thread::spawn};
+
+use tungstenite::{accept, Message};
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("localhost:9001").unwrap();
+    for stream in server.incoming() {
+        spawn(move || {
+            let mut websocket = accept(stream.unwrap()).unwrap();
+            loop {
+                let msg = websocket.read().unwrap();
+
+                match msg {
+                    Message::Binary(bin) => {
+                        println!("incoming msg {:} bytes", bin.len());
+
+                        websocket.send(Message::Binary(bin)).unwrap()
+                    }
+                    _ => {}
+                }
+            }
+        });
+    }
 }
